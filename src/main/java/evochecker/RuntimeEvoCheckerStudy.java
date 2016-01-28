4bcd9071434f0ee971d0cfd9a8fdbfb08402422f
Simos Gerasimou <simos@cs.york.ac.uk>
2016-01-28 16:10:16 +0000

    - added runtimeEvochecker study

---
+++ src/main/java/evochecker/RuntimeEvoCheckerStudy.java
@@ -0,0 +1,28 @@
+package evochecker;
+
+import evochecker.auxiliary.Utility;
+
+public class RuntimeEvoCheckerStudy {
+
+	
+	public static void main(String[] args) {
+		EvoChecker evoChecker = new EvoChecker();
+
+		int MAXRUNS = 30;
+
+		String seedings[] = new String[] {"NORMAL", "BEST", "POPULATION"};
+		
+		//for all the seeding techniques
+		for (String seeding : seedings){
+			Utility.setProperty("SEEDING", seeding);
+			System.out.println(Utility.getProperty("SEEDING"));
+
+			//run for MAXRUNS
+			for (int run=0; run<MAXRUNS; run++){
+				evoChecker.main(null);
+			}
+		}
+		
+	}
+
+}
