3e3bf4ab5fefd21cd8b31fbd977ffe8e067849ac
Simos Gerasimou <simos@cs.york.ac.uk>
2016-02-03 17:38:18 +0000

    - dummy tests

---
+++ src/test/java/Templates/DPMTest.java
@@ -0,0 +1,17 @@
+package Templates;
+
+import org.junit.Assert;
+
+import evochecker.EvoChecker;
+
+public class DPMTest {
+
+	public void testPrintHelloWorld() {
+		//Testing Class
+		EvoChecker evoChecker = new EvoChecker();
+		
+//		evoChecker.
+//		Assert.assertEquals(EvoChecker.);
+
+	}
+}
---
+++ src/test/java/Templates/FXTemplate.java
@@ -0,0 +1,19 @@
+package Templates;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FXTemplate {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
