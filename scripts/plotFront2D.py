837614351d4ced736b5047d9e56beb9bd0c353b1
Simos Gerasimou <simos@cs.york.ac.uk>
2020-10-28 19:26:40 +0000

    added EvoCheckerRunner

---
+++ scripts/plotFront2D.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import sys
+
+
+paretoFront = str(sys.argv[1])
+
+f = pd.read_csv(paretoFront, header=0, delimiter="\t")
+
+plt.scatter(f.iloc[:,0], f.iloc[:,1])# s=area, c=colors, alpha=0.5)
+plt.xlabel(f.columns[0])
+plt.ylabel(f.columns[1])
+plt.show()
+
+print("Done")
---
+++ src/examples/EvoCheckerRunner.java
@@ -0,0 +1,20 @@
+package examples;
+
+import evochecker.EvoChecker;
+
+public class EvoCheckerRunner {
+
+	public static void main(String[] args) {
+		//1) Create EvoChecker instance
+		
+		EvoChecker ec = new EvoChecker();
+		
+		//2) Set configuration file
+		String configFile ="resources/config.properties"; 
+		ec.setConfigurationFile(configFile);
+		
+		//3) Start EvoChecker
+		ec.start();		
+	}
+
+}
