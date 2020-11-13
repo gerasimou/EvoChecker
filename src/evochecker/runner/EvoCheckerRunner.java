d726031ba782b83e071048debbb98eb40e728a83
Simos Gerasimou <simos@cs.york.ac.uk>
2020-11-13 17:32:48 +0000

    - Updated jar

---
+++ src/evochecker/runner/EvoCheckerRunner.java
@@ -0,0 +1,19 @@
+package evochecker.runner;
+
+import evochecker.EvoChecker;
+
+public class EvoCheckerRunner {
+
+	public static void main(String[] args) {
+		//1) Create EvoChecker instance
+		EvoChecker ec = new EvoChecker();
+		
+		//2) Set configuration file
+		String configFile ="config.properties"; 
+		ec.setConfigurationFile(configFile);
+		
+		//3) Start EvoChecker
+		ec.start();
+	}
+
+}
